3bd4d86ed24ace210897d9fe718ef325bfebb816
lfelipe <rafael.eng.mecanic@gmail.com>
2021-11-08 19:14:50 -0300

    Excptions

--- src/main/java/pokemon/api/crud/resources/PokemonResoruce.java
+++ src/main/java/pokemon/api/crud/resources/PokemonResoruce.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pokemon.api.crud.dto.PokemonDTO;
 import pokemon.api.crud.services.PokemonService;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/pokemons")
@@ -23,5 +26,7 @@ public class PokemonResoruce {
 
         return ResponseEntity.ok().body(list);
     }
+
+
 
 }
